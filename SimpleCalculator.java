655078f60922d78cf629ece91a2c64ceb05902ff
yugandhar1212 <yogiyogandhar05@gmail.com>
2025-04-15 15:15:14 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
-
 public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Ask user to enter two numbers
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
-
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
-
-        // Ask user to enter an operator
         System.out.print("Enter an operator (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
 
         double result;
-
-        // Perform the calculation
         switch (operator) {
             case '+':
                 result = num1 + num2;
@@ -39,13 +30,10 @@ public class SimpleCalculator {
                     return;
                 }
                 break;
-
             default:
                 System.out.println("Invalid operator!");
                 return;
         }
-
-        // Display the result
         System.out.println("Result: " + result);
     }
 }
